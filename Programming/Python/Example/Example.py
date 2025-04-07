e087b53c564adadd843553a23e8dfe9eebefb144
lkstudio.dante <lkstudio.dante@gmail.com>
2025-04-08 02:05:54 +0900

    no message

--- Programming/Python/Example/Example.py
+++ Programming/Python/Example/Example.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-from _03410122000101_KITA_Weekday_Python_0001.E01.Scripts.Runtime.Example.Example_01 import E01Example_01
-from _03410122000101_KITA_Weekday_Python_0001.E01.Scripts.Runtime.Example.Example_02 import E01Example_02
-from _03410122000101_KITA_Weekday_Python_0001.E01.Scripts.Runtime.Example.Example_03 import E01Example_03
-from _03410122000101_KITA_Weekday_Python_0001.E01.Scripts.Runtime.Example.Example_04 import E01Example_04
+from _03410122000101_KoreaIT_Weekday_Python_0001.Programming.E01.Scripts.Runtime.Example.Example_01 import P_E01Example_01
+from _03410122000101_KoreaIT_Weekday_Python_0001.Programming.E01.Scripts.Runtime.Example.Example_02 import P_E01Example_02
+from _03410122000101_KoreaIT_Weekday_Python_0001.Programming.E01.Scripts.Runtime.Example.Example_03 import P_E01Example_03
+from _03410122000101_KoreaIT_Weekday_Python_0001.Programming.E01.Scripts.Runtime.Example.Example_04 import P_E01Example_04
 
 # 메인 모듈 일 경우
 if __name__ == "__main__":
-	E01Example_01.start(sys.argv)
-	# E01Example_02.start(sys.argv)
-	# E01Example_03.start(sys.argv)
-	# E01Example_04.start(sys.argv)
+	P_E01Example_01.start(sys.argv)
+	# P_E01Example_02.start(sys.argv)
+	# P_E01Example_03.start(sys.argv)
+	# P_E01Example_04.start(sys.argv)
